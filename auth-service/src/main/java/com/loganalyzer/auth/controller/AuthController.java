7da8d82c70bee5fa28b8589da26f6814ed638e19
Ritesh <ritesh.kedar06@gmail.com>
2026-06-01 12:24:55 +0530

    Added Swagger and OpenAPI Doc

--- auth-service/src/main/java/com/loganalyzer/auth/controller/AuthController.java
+++ auth-service/src/main/java/com/loganalyzer/auth/controller/AuthController.java
@@ -6,10 +6,16 @@ import com.loganalyzer.auth.dto.request.RefreshTokenRequest;
 import com.loganalyzer.auth.dto.request.RegisterRequest;
 import com.loganalyzer.auth.dto.response.ApiResponse;
 import com.loganalyzer.auth.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(
+        name = "Authentication",
+        description = "Authentication APIs"
+)
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
@@ -20,6 +26,9 @@ public class AuthController {
         this.authService = authService;
     }
 
+    @Operation(
+            summary = "Register User"
+    )
     @PostMapping("/register")
     public ResponseEntity<ApiResponse> register(
             @Valid @RequestBody RegisterRequest request
@@ -27,6 +36,10 @@ public class AuthController {
         return ResponseEntity.ok(authService.register(request));
     }
 
+    @Operation(
+            summary = "Login User",
+            description = "Authenticate user and return access and refresh tokens"
+    )
     @PostMapping("/login")
     public ResponseEntity<ApiResponse> login(
             @Valid @RequestBody LoginRequest request
@@ -34,6 +47,10 @@ public class AuthController {
         return ResponseEntity.ok(authService.login(request));
     }
 
+    @Operation(
+            summary = "Refresh Token",
+            description = "Refresh the Token"
+    )
     @PostMapping("/refresh")
     public ResponseEntity<ApiResponse> refreshToken(
             @Valid
@@ -47,6 +64,10 @@ public class AuthController {
                 )
         );
     }
+
+    @Operation(
+            summary = "Logout User"
+    )
     @PostMapping("/logout")
     public ResponseEntity<ApiResponse> logout(
             @Valid
